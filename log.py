c522e12a0bda48e316e2df97ebf7310870ce4514
Mrfoogles <friedrich.hohensee@gmail.com>
2021-03-05 16:52:13 -0800

    Just move the files in the worlds directory to the main directory

--- log.py
+++ log.py
@@ -1,11 +1,5 @@
 # An example world, not doing much but extending the base class and logging events
 
-# Necessary to import files from the parent directory (yes, I tried adding __init__.py files)
-# prints 'ImportError: attempted relative import with no known parent package' without this
-# from stackoverflow
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import core
 from util import *
 import pygame as pg
